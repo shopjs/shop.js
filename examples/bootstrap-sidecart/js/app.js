c4879cd73a356cc4e84861f81b68400d06d62b30
Thomas <jtruant@gmx.com>
2016-04-17 23:49:59 -0600

    init

---
+++ examples/bootstrap-sidecart/js/app.js
@@ -0,0 +1,16 @@
+var Shop = require('./shop.js');
+
+Shop.use({
+  Controls: {
+    Error: '<div class="bootstrap classes" if="{ errorMessage}">{ errorMessage }</div>'
+  }
+});
+
+var m = Shop.start(require('./settings'));
+
+window.m = m;
+
+$('.buy-button').on('click', (event) => {
+  if(!Shop.getItem('droney'))
+    Shop.setItem('droney', 1);
+});
