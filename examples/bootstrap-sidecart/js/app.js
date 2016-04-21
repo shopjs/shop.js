3d7b12b4307cd67c16ca642b15d6f3937f59a6c7
Thomas <jtruant@gmx.com>
2016-04-20 20:46:33 -0600

    hide/show .side-cart

--- examples/bootstrap-sidecart/js/app.js
+++ examples/bootstrap-sidecart/js/app.js
@@ -11,7 +11,17 @@ var m = Shop.start(require('./settings'));
 
 window.m = m;
 
-$('.buy-button').on('click', (event) => {
+$('.buy-button').on('click', function(event) {
   if(!Shop.getItem('droney'))
     Shop.setItem('droney', 1);
+  // show cart
+  $('.side-cart').removeClass('hidden');
+  // disable checkout button
+  $(this).attr('disabled', true);
+});
+
+// this button will only be viewable after .buy-button has been clicked.
+$('#back').on('click', event => {
+  $('.side-cart').addClass('hidden');
+  $('.buy-button').attr('disabled', false);
 });
