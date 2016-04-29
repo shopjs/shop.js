792c2cac2bc5dcef71734c9d628341472c2a8753
Thomas <jtruant@gmx.com>
2016-04-28 21:29:24 -0600

    wrap in doc.rdy

--- examples/bootstrap-sidecart/js/app.js
+++ examples/bootstrap-sidecart/js/app.js
@@ -1,31 +1,34 @@
 
 var Shop = require('shop.js');
 
-Shop.use({
-  Controls: {
-    Error: '<div class="bootstrap classes" if="{ errorMessage}">{ errorMessage }</div>'
-  }
+$(document).ready(() => {
+  Shop.use({
+    Controls: {
+      Error: '<div class="bootstrap classes" if="{ errorMessage }">{ errorMessage }</div>'
+    }
+  });
+
+  var m = Shop.start(require('./settings'));
+
+  window.m = m;
+
+  // /index.html
+  $('.buy-button').on('click', function(event) {
+    if(!Shop.getItem('droney'))
+      Shop.setItem('droney', 1);
+    // show cart
+    $('.side-cart').removeClass('hidden');
+    // disable checkout button
+    $(this).attr('disabled', true);
+  });
+
+  // this button will only be viewable after .buy-button has been clicked.
+  $('#back').on('click', event => {
+    $('.side-cart').addClass('hidden');
+    $('.buy-button').attr('disabled', false);
+  });
+
+  $('#checkout').on('click', event => {
+    window.location = 'checkout';
+  })
 });
-
-var m = Shop.start(require('./settings'));
-
-window.m = m;
-
-$('.buy-button').on('click', function(event) {
-  if(!Shop.getItem('droney'))
-    Shop.setItem('droney', 1);
-  // show cart
-  $('.side-cart').removeClass('hidden');
-  // disable checkout button
-  $(this).attr('disabled', true);
-});
-
-// this button will only be viewable after .buy-button has been clicked.
-$('#back').on('click', event => {
-  $('.side-cart').addClass('hidden');
-  $('.buy-button').attr('disabled', false);
-});
-
-$('#checkout').on('click', event => {
-  window.location = 'checkout';
-})
