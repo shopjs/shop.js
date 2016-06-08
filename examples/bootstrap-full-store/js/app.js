deacbd325d71567be12ff5597655b91de1cd768d
Thomas <jtruant@gmx.com>
2016-06-07 20:38:19 -0600

    start port to full page

--- examples/bootstrap-full-store/js/app.js
+++ examples/bootstrap-full-store/js/app.js
@@ -30,16 +30,6 @@ $(window).load(() => {
   const m = Shop.start(require('./settings'));
 
   //
-  // The 'ready' event is sent when Shop.js has finished its first rendering and all the dynamic content is loaded
-  //
-  m.on('ready', () => {
-    //
-    // Remove the content loading class to hide the loading spinner and show the dynamic content.
-    //
-    $('.loading').removeClass('loading');
-  });
-
-  //
   // The 'submit-success' event is sent when the user successfully submits a payment and it is successfully charged.
   //
   m.on('submit-success', () => {
@@ -60,63 +50,6 @@ $(window).load(() => {
       window.location = '/';
     }
   }
-
-  //
-  // The .buy-button is on index.html and opens the side cart
-  //
-  $buyButton = $('.buy-button');
-
-  //
-  // .back-button is on the index.html and closes the side cart
-  //
-  $backButton = $('.back-button');
-
-  //
-  // The .side-cart is on index.html
-  //
-  $sideCart = $('.side-cart');
-
-  //
-  // The .checkoutbtn is on index.html and submits the cart to checkout
-  //
-  $checkoutButton = $('.checkout-button')
-
-  //
-  // When the user clicks the .buy-button, add a Droney 2.0 to the user's cart, and then show the
-  // side cart.
-  //
-  $buyButton.on('click', (event) => {
-    //
-    // If there is no Droney item of any type in the cart, add it 1.
-    //
-    if(!Shop.getItem('droney-2.0'))
-      Shop.setItem('droney-2.0', 1);
-
-    //
-    // Show the side cart
-    //
-    $sideCart.removeClass('hidden');
-
-    //
-    // Disable the .buy-button
-    //
-    $buyButton.attr('disabled', true);
-  });
-
-  //
-  // When the user clicks .back-button, close the side cart
-  //
-  $backButton.on('click', event => {
-    //
-    // Hide the side cart
-    //
-    $sideCart.addClass('hidden');
-
-    //
-    // Enable the .buy-button
-    //
-    $buyButton.attr('disabled', false);
-  });
 
   //
   // When the user clicks the .checkout-btn, redirect the user to the checkout page.
