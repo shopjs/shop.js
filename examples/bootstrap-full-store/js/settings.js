fb0672886a146d8545fedbcea2e605173c1d02b4
Thomas <jtruant@gmx.com>
2016-06-07 19:58:54 -0600

    copy usable files from sidecart example

---
+++ examples/bootstrap-full-store/js/settings.js
@@ -0,0 +1,17 @@
+module.exports = {
+  key: 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJiaXQiOjQ1MDM2MTcwNzU2NzUxNzYsImp0aSI6IkNTaWFDckhpdDQ0Iiwic3ViIjoiRXFUR294cDV1MyJ9.fRcRQRRe0CrcnGSW12fmQ_8Cly6mqByIc5wTnANPdPWP3V1Bx9AIGbTVPTx_3KbBEziGewKJtNT1ys6WDXegyg',
+  endpoint: 'https://api.staging.crowdstart.com',
+  taxRates: [
+    {
+      taxRate: 0.0875,
+      city: 'san francisco',
+      state:    'ca',
+      country: 'us'
+    },
+    {
+      taxRate: 0.075,
+      state:   'ca',
+      country: 'us'
+    }
+  ]
+};
