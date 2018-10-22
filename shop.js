eea2b696bc3c1d974d2410ee247dc0946b2f22f6
David Tai <david@verus.io>
2018-10-22 16:31:19 -0500

    rebuild

--- shop.js
+++ shop.js
@@ -10920,8 +10920,7 @@ var Shop = (function () {
             : Interpolation.Linear;
 
         if (typeof end === 'number') {
-          object[property] =
-            (((start + (end - start) * value) * DECIMAL) | 0) / DECIMAL;
+          object[property] = start + (end - start) * value;
         } else if (Array.isArray(end) && !Array.isArray(start)) {
           object[property] = _interpolationFunctionCall(
             end,
