c819b7ee55125763f65eeb01bd00a5343df85f84
David Tai <david@verus.io>
2020-03-06 23:05:12 -0600

    update to use static store

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,2 +1,20 @@
+import ShopStore, { ILibraryClient } from './ShopStore'
+
 export { default as ShopStore } from './ShopStore'
 export * from './ShopStore'
+
+let store: ShopStore | undefined
+
+const getStore = (client?: ILibraryClient, opts = {}): ShopStore | undefined => {
+  if (!client) {
+    return
+  }
+
+  if (!store) {
+    store = new ShopStore(client, opts)
+  }
+
+  return store
+}
+
+export default getStore
