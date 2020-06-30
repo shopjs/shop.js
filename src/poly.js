38229ac1fbb9f1db321a37e6bb79053f23b251e5
David Tai <david@verus.io>
2020-06-29 19:58:27 -0500

    fix polyfill window

--- src/poly.js
+++ src/poly.js
@@ -1,13 +1,15 @@
-if (window.Element && !Element.prototype.closest) {
-  Element.prototype.closest =
-  function(s) {
-    var matches = (this.document || this.ownerDocument).querySelectorAll(s),
-        i,
-        el = this;
-    do {
-      i = matches.length;
-      while (--i >= 0 && matches.item(i) !== el) {};
-    } while ((i < 0) && (el = el.parentElement));
-    return el;
-  };
+if (typeof window !== 'undefined') {
+  if (window.Element && !Element.prototype.closest) {
+    Element.prototype.closest =
+    function(s) {
+      var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+          i,
+          el = this;
+      do {
+        i = matches.length;
+        while (--i >= 0 && matches.item(i) !== el) {};
+      } while ((i < 0) && (el = el.parentElement));
+      return el;
+    };
+  }
 }
