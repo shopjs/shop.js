19eb2b564f210f6988f5f3cab612b549699e56df
David Tai <david@verus.io>
2020-06-29 19:58:27 -0500

    fix polyfill window

--- dist/index.umd.js
+++ dist/index.umd.js
@@ -70215,17 +70215,19 @@
 	    return (React__default.createElement("div", null, count));
 	};
 
-	if (window.Element && !Element.prototype.closest) {
-	  Element.prototype.closest =
-	  function(s) {
-	    var matches = (this.document || this.ownerDocument).querySelectorAll(s),
-	        i,
-	        el = this;
-	    do {
-	      i = matches.length;
-	      while (--i >= 0 && matches.item(i) !== el) {}    } while ((i < 0) && (el = el.parentElement));
-	    return el;
-	  };
+	if (typeof window !== 'undefined') {
+	  if (window.Element && !Element.prototype.closest) {
+	    Element.prototype.closest =
+	    function(s) {
+	      var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+	          i,
+	          el = this;
+	      do {
+	        i = matches.length;
+	        while (--i >= 0 && matches.item(i) !== el) {}      } while ((i < 0) && (el = el.parentElement));
+	      return el;
+	    };
+	  }
 	}
 
 	/*! *****************************************************************************
