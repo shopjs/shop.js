95f785e7104817de2ff7e1ae062dcd6a67ba11da
David Tai <david@verus.io>
2020-07-20 10:07:01 -0500

    add typescript poly

--- dist/index.umd.js
+++ dist/index.umd.js
@@ -70210,18 +70210,17 @@
 	};
 
 	if (typeof window !== 'undefined') {
-	  if (window.Element && !Element.prototype.closest) {
-	    Element.prototype.closest =
-	    function(s) {
-	      var matches = (this.document || this.ownerDocument).querySelectorAll(s),
-	          i,
-	          el = this;
-	      do {
-	        i = matches.length;
-	        while (--i >= 0 && matches.item(i) !== el) {}      } while ((i < 0) && (el = el.parentElement));
-	      return el;
-	    };
-	  }
+	    if (window.Element && !Element.prototype.closest) {
+	        Element.prototype.closest =
+	            function (s) {
+	                var matches = (this.document || this.ownerDocument).querySelectorAll(s), i, el = this;
+	                do {
+	                    i = matches.length;
+	                    while (--i >= 0 && matches.item(i) !== el) { }
+	                } while ((i < 0) && (el = el.parentElement));
+	                return el;
+	            };
+	    }
 	}
 
 	/*! *****************************************************************************
