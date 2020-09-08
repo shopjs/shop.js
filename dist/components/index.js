7e44626cd190539876d57726555f1ff0a238ad7e
David Tai <david@verus.io>
2020-09-08 13:49:53 -0500

    set coupon update form commerce.js

--- dist/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as ShippingForm } from './ShippingForm';
-export { default as PaymentForm } from './PaymentForm';
-export { default as Checkout } from './Checkout';
-export { default as Cart } from './Cart';
-export { default as CartCount } from './CartCount';
